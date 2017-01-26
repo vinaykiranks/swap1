2b8907ce7422df6d2c12a9b8fed22c67a59e8850
vinaykiranks <vinaykiranks886@gmail.com>
2017-01-27 00:15:43 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,23 @@
+public class swap
+{
+
+ public static void main(String s[])
+  
+  {
+
+
+    int a=10;
+     int b=20;
+System.out.println("before swap");
+System.out.println("a="+a);
+System.out.println("b="+b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("after swamp");
+System.out.println("a="+a);
+System.out.println("b="+b);
+
+}
+
+}
